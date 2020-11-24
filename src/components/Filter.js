dc4034aa7f742d47bd677036aca7d1531b48675a
Jihee Hong <486480@student.saxion.nl>
2020-11-24 10:56:35 +0100

    add redux to filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,24 @@
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import { filterProducts, sortProducts } from '../actions/productActions';
 
-export default class Filter extends Component {
+class Filter extends Component {
   render() {
-    return (
+    return !this.props.filteredProducts ? (
+      <div>Loading...</div>
+    ) : (
       <div className='filter'>
-        <div className='filter-result'>{this.props.count} Products</div>
+        <div className='filter-result'>
+          {this.props.filteredProducts.length} Products
+        </div>
         <div className='filter-sort'>
           Order{' '}
-          <select value={this.props.sort} onChange={this.props.sortProducts}>
+          <select
+            value={this.props.sort}
+            onChange={(e) =>
+              this.props.sortProducts(this.props.products, e.target.value)
+            }
+          >
             <option>Latest</option>
             <option value='lowest'>Lowest</option>
             <option value='highest'>Highest</option>
@@ -15,7 +26,12 @@ export default class Filter extends Component {
         </div>
         <div className='filter-size'>
           Filter{' '}
-          <select value={this.props.size} onChange={this.props.filterProducts}>
+          <select
+            value={this.props.size}
+            onChange={(e) =>
+              this.props.filterProducts(this.props.products, e.target.value)
+            }
+          >
             <option>All</option>
             <option value='XS'>XS</option>
             <option value='S'>S</option>
@@ -29,3 +45,16 @@ export default class Filter extends Component {
     );
   }
 }
+
+export default connect(
+  (state) => ({
+    size: state.products.size,
+    sort: state.products.sort,
+    products: state.products.items,
+    filteredProducts: state.products.filteredItems,
+  }),
+  {
+    filterProducts,
+    sortProducts,
+  }
+)(Filter);
